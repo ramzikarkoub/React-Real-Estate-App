a19c32b1fbfa7b7486058239180768815f127342
Ramzi Karkoub <karkoub.ramzi@gmail.com>
2025-03-07 21:33:01 -0500

    prepare to deploy

--- src/api/apiRequest.js
+++ src/api/apiRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiRequest = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true,
+  withCredentials: true, // send cookies
 });
 
 export default apiRequest;
