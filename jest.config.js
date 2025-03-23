3bd82746cdacea182ec3f30f845ad5dc65b178fc
Ramzi Karkoub <karkoub.ramzi@gmail.com>
2025-03-22 23:15:24 -0400

    add testing

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+export default {
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  moduleFileExtensions: ["js", "jsx"],
+};
