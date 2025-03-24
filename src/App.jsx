2f93fdfe5db728f842d855b0cc06d0894e1092a2
Ramzi Karkoub <karkoub.ramzi@gmail.com>
2025-03-24 00:55:08 -0400

    Temporarily rename route folders to workaround case-sensitive rename

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-
 import ProtectedRoute from "./api/ProtectedRoute";
 import Dashboard from "./routes/Dashboard/Dashboard";
 import Layout from "./components/Layout/Layout";
 import HomePage from "./routes/HomePage/HomePage";
-import BuyPage from "./routes/BuyPage/BuyPage";
 import RentPage from "./routes/RentPage/RentPage";
 import SinglePostPage from "./routes/SinglePost/SinglePost";
 import LoginPage from "./routes/Login/Login";
 import RegisterPage from "./routes/Register/Register";
+import BuyPage from "./routes/BuyPage/BuyPage";
 
 const router = createBrowserRouter([
   {
