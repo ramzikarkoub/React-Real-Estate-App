ac39eb996695fc3aae4b43bdbe39e1c146b7a4ee
Ramzi Karkoub <karkoub.ramzi@gmail.com>
2025-03-26 00:54:55 -0400

    chore: temp rename searchBar to searchBar_temp

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useMemo } from "react";
 import { Outlet, useLocation } from "react-router-dom";
-import Footer from "../footer/Footer";
+import Footer from "../Footer/Footer";
 import SearchBar from "../SearchBar/SearchBar.jsx";
 import PostContext from "../../context/PostContext";
 import UserContext from "../../context/UserContext";
